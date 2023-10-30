b4621efa5669033ac7175a1e66042606248bfea6
samuel <samuel@carolyn.my.domain>
2023-10-30 11:21:38 +0530

    Add lsp-mode for C++

--- init.el
+++ init.el
@@ -7,6 +7,7 @@
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
+(tab-bar-mode 1)
 (display-time)
 (server-start)
 
@@ -21,64 +22,9 @@
 (setenv "LANGUAGE" "en_US.UTF-8")
 (setenv "LANG" "en_US.UTF-8")
 
-(setq JAVA_BASE "/Library/Java/JavaVirtualMachines")
-
-;;
-;; This function returns the list of installed
-;;
-(defun switch-java--versions ()
-  "Return the list of installed JDK."
-  (seq-remove
-   (lambda (a) (or (equal a ".") (equal a "..")))
-   (directory-files JAVA_BASE)))
-
-
-(defun switch-java--save-env ()
-  "Store original PATH and JAVA_HOME."
-  (when (not (boundp 'SW_JAVA_PATH))
-    (setq SW_JAVA_PATH (getenv "PATH")))
-  (when (not (boundp 'SW_JAVA_HOME))
-    (setq SW_JAVA_HOME (getenv "JAVA_HOME"))))
-
-
-(defun switch-java ()
-  "List the installed JDKs and enable to switch the JDK in use."
-  (interactive)
-  ;; store original PATH and JAVA_HOME
-  (switch-java--save-env)
-
-  (let ((ver (completing-read
-              "Which Java: "
-              (seq-map-indexed
-               (lambda (e i) (list e i)) (switch-java--versions))
-              nil t "")))
-    ;; switch java version
-    (setenv "JAVA_HOME" (concat JAVA_BASE "/" ver "/Contents/Home"))
-    (setenv "PATH" (concat (concat (getenv "JAVA_HOME") "/bin/java")
-                           ":" SW_JAVA_PATH)))
-  ;; show version
-  (switch-java-which-version?))
-
-
-(defun switch-java-default ()
-  "Restore the default Java version."
-  (interactive)
-  ;; store original PATH and JAVA_HOME
-  (switch-java--save-env)
-
-  ;; switch java version
-  (setenv "JAVA_HOME" SW_JAVA_HOME)
-  (setenv "PATH" SW_JAVA_PATH)
-  ;; show version
-  (switch-java-which-version?))
-
-
-(defun switch-java-which-version? ()
-  "Display the current version selected Java version."
-  (interactive)
-  ;; displays current java version
-  (message (concat "Java HOME: " (getenv "JAVA_HOME"))))
-
+(setq gc-cons-threshold 100000000)
+(set-charset-priority 'unicode)
+(prefer-coding-system 'utf-8-unix)
 
 (require 'package)
 
@@ -89,6 +35,8 @@
 	     '("marmalade" . "https://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
 	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives 
+             '("nongnu" . "https://elpa.nongnu.org/nongnu/"))
 (add-to-list 'package-archives
 	     '("gnu" . "https://elpa.gnu.org/packages/") t)
 
@@ -102,9 +50,6 @@
 (when (not (package-installed-p 'use-package))
   (package-refresh-contents)
   (package-install 'use-package))
-
-
-;; This `setq-default` form adapted from https://github.com/suvratapte/dot-emacs-dot-d/blob/master/init.el
 
 
 (defun cider-repl-prompt-on-newline (ns)
@@ -123,9 +68,6 @@
  inhibit-startup-message t
  fzf/executable "/usr/local/bin/fzf"
 
- ;; Do not put 'customize' config in init.el; give it another file.
- ;; custom-file "~/.emacs.d/custom-file.el"
-
  ns-right-command-modifier 'hyper
  ns-right-option-modifier 'super
 
@@ -137,7 +79,6 @@
 ;; Automatically update buffers if file content on the disk has changed.
 ;; (global-auto-revert-mode t)
 
-
 (global-set-key (kbd "H-2") 'split-window-below)
 (global-set-key (kbd "H-3") 'split-window-right)
 
@@ -148,6 +89,10 @@
 (global-set-key (kbd "H-M-f") 'flip-frame)
 (global-set-key (kbd "C-s-f") 'transpose-frame)
 
+(global-set-key (kbd "s-w") 'ace-window)
+(global-set-key (kbd "s-k") 'kill-buffer)
+ 
+(global-set-key (kbd "H-/") 'comment-or-uncomment-region)
 
 (global-set-key (kbd "H-r") 'window-configuration-to-register)
 (global-set-key (kbd "H-e") 'jump-to-register)
@@ -174,7 +119,7 @@
 (global-set-key (kbd "H-M-g") 'ace-jump-word-mode)
 (global-set-key (kbd "H-g") 'ace-jump-char-mode)
 
-;; (global-set-key (kbd "H-M-d") 'fzf-directory)
+(global-set-key (kbd "H-M-d") 'fzf-directory)
 (global-set-key (kbd "H-s") 'save-buffer)
 
 (global-set-key (kbd "s-s") 'org-clock-in)
@@ -184,7 +129,6 @@
 
 (global-set-key (kbd "H-b") 'ido-switch-buffer)
 
-
 (global-set-key (kbd "s-c") 'cider-connect-clj)
 (global-set-key (kbd "s-z") 'cider-switch-to-repl-buffer)
 (global-set-key (kbd "s-n") 'cider-repl-set-ns)
@@ -192,9 +136,7 @@
 
 (global-set-key (kbd "H-l") 'cider-load-buffer)
 (global-set-key (kbd "H-k") 'cider-eval-defun-at-point)
-
 (global-set-key (kbd "H-<f3>") 'cider-debug-defun-at-point)
-
 (global-set-key (kbd "H-t") 'cider-test-run-test)
 (global-set-key (kbd "H-M-t") 'cider-test-run-ns-tests)
 
@@ -222,35 +164,33 @@
   :config
   (load-theme 'nord t))
 
-
 (use-package deadgrep
   :ensure t)
 
-
 (use-package magit
-  :ensure t)
-
-
-(use-package lsp-java
   :ensure t
-  :init
-  (add-hook 'java-mode-hook #'lsp))
-
-
-(use-package magit-delta
   :ensure t
-  :init
-  (add-hook 'magit-mode-hook (lambda () (magit-delta-mode +1))))
+  :bind ("C-x g" . magit-status))
 
+;; (use-package lsp-java
+;;   :ensure t
+;;   :init
+;;   (add-hook 'java-mode-hook #'lsp))
+
+;; (use-package magit-delta
+;;   :ensure t
+;;   :init
+;;   (add-hook 'magit-mode-hook (lambda () (magit-delta-mode +1))))
 
 (use-package fzf
   :ensure t)
-
 
 (use-package raku-mode
   :ensure t
   :defer t)
 
+(use-package nix-mode
+  :ensure t)
 
 (defun my-cljr-clojure-mode-hook ()
   (clj-refactor-mode 1)
@@ -262,7 +202,8 @@
   :init
   (add-hook 'clojure-mode-hook #'my-cljr-clojure-mode-hook))
 
-
+;; A few of these configurations have been adapted from
+;; Mr. S.U.V.R.A.T Apte.
 (use-package paredit
   :ensure t
   :init
@@ -281,53 +222,39 @@
   :bind (("M-[" . paredit-wrap-square)
 	 ("M-{" . paredit-wrap-curly)))
 
-
 (use-package eldoc
   :config
   (global-eldoc-mode t))
-
 
 (use-package company
   :ensure t
   :init
   (add-hook 'after-init-hook 'global-company-mode))
 
-
-(use-package lsp-mode
-  :ensure t
-  :init
-  (setq lsp-keymap-prefix "s-r")
-  :hook
-  ((rust-mode . lsp)
-   ;; (clojure-mode . lsp)
-   (lsp-mode . lsp-enable-which-key-integration)
-   (java-mode . #'lsp-deferred))
-  :config
-  (dolist (m '(clojure-mode
-               clojurec-mode
-               clojurescript-mode
-               clojurex-mode))
-    (add-to-list 'lsp-language-id-configuration `(,m . "clojure")))
-  :commands lsp)
+;; (use-package lsp-mode
+;;   :ensure t
+;;   :init
+;;   (setq lsp-keymap-prefix "s-r")
+;;   :hook
+;;   ((rust-mode . lsp)
+;;    ;; (clojure-mode . lsp)
+;;    (lsp-mode . lsp-enable-which-key-integration)
+;;    (java-mode . #'lsp-deferred))
+;;   :config
+;;   (dolist (m '(clojure-mode
+;;                clojurec-mode
+;;                clojurescript-mode
+;;                clojurex-mode))
+;;     (add-to-list 'lsp-language-id-configuration `(,m . "clojure")))
+;;   :commands lsp)
 
 
 (use-package rust-mode
   :ensure t)
 
 
-(use-package racer
-  :ensure t
-  :init
-  (add-hook 'rust-mode-hook #'racer-mode)
-  (add-hook 'racer-mode-hook #'eldoc-mode)
-  (add-hook 'racer-mode-hook #'company-mode)
-  :config
-  (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common))
-
-
 (use-package markdown-mode
   :ensure t)
-
 
 (use-package ivy
   :ensure t
@@ -344,13 +271,11 @@
   :bind (("C-x b" . ivy-switch-buffer)
 	 ("C-x B" . ivy-switch-buffer-other-window)))
 
-
 (use-package swiper
   :ensure t
   :bind (("C-s" . swiper-isearch)
 	 ("C-M-s" . isearch-forward-regexp)
 	 ("C-M-r" . isearch-backward-regexp)))
-
 
 (use-package counsel
   :ensure t
@@ -360,13 +285,6 @@
 	 ("C-c s" . counsel-rg)
 	 :map counsel-find-file-map
 	 ("RET" . ivy-alt-done)))
-
-
-(use-package ace-jump-mode
-  :ensure t
-  :bind (("C-." . ace-jump-mode)
-	 ("C-c l" . ace-jump-line-mode)))
-
 
 (use-package which-key
   :ensure t
@@ -393,13 +311,24 @@
 
 
 (use-package sly
-  :ensure t)
+  :ensure t
+  :bind ("C-<return>" . sly-mrepl-return))
 
 ;; (unless (package-installed-p 'clojure-mode)
 ;;   (package-install 'clojure-mode))
 (use-package clojure-mode
   :ensure t)
 
+(defun lsp-go-install-save-hooks ()
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+
+(use-package go-mode
+  :ensure t
+  :config (progn
+	    (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+	    (add-hook 'go-mode-hook #'lsp-deferred)
+	    (add-hook 'go-mode-hook #'yas-minor-mode)))
 
 (use-package cider
   :ensure t)
@@ -430,6 +359,34 @@
 		      (clj-kondo-edn . edn-joker)))
     (flycheck-add-next-checker (car checkers) (cons 'error (cdr checkers)))))
 
+;; (use-package lsp-mode
+;;   :ensure t
+;;   :init
+;;   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+;;   (setq lsp-keymap-prefix "s-l")
+;;   :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+;;          (c-mode . lsp)
+;;          ;; if you want which-key integration
+;;          (lsp-mode . lsp-enable-which-key-integration))
+;;   :commands lsp)
+
+
+;; (use-package ccls
+;;   :ensure t
+;;   :init (setq ccls-sem-highlight-method 'font-lock)
+;;   :hook ((c-mode c++-mode objc-mode) . (lambda () (require 'ccls) (lsp))))
+
+
+;; (require 'dired-x)
+
+;; (add-hook 'slime-repl-mode-hook 'enable-paredit-mode)
+;; (add-hook 'slime-mode-hook (lambda () (slime-autodoc-mode t)))
+;; (add-hook 'lisp-mode-hook (lambda () (paredit-mode +1)))
+;; (add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1)))
+;; (add-hook 'slime-connected-hook (lambda () (paredit-mode +1)))
+;; (add-hook 'slime-mode-hook            'turn-on-eldoc-mode)
+;; (add-hook 'lisp-mode-hook             'turn-on-eldoc-mode)
+;; (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
 
 ;; (setq-default indent-tabs-mode nil
 ;;               ;; dired-omit-files-p t
@@ -451,24 +408,26 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-refile-targets '((org-agenda-files :maxlevel . 3)))
+ '(indent-tabs-mode nil)
+ '(org-agenda-files
+   '("~/projects/hiring/qube/shortest-path/plan.org" "/home/samuel/org/tech.org" "/home/samuel/org/tasks.org"))
  '(package-selected-packages
-   '(nix-mode lsp-java racer racer-mode clj-refactor markdown-mode which-key use-package transpose-frame swoop sly rg raku-mode projectile perlbrew paredit nord-theme magit-delta fzf flycheck-joker flycheck-clj-kondo elpher dumb-jump deadgrep counsel company cider ace-jump-mode))
- '(warning-suppress-types '((comp))))
-
+   '(go-mode ace-window 0blayout ac-capf lua-mode rust-mode cmake-mode catppuccin-theme nix-mode sly-macrostep ox-gemini gemini-mode ccls lsp-mode flycheck-clj-kondo flycheck-joker flycheck vc-fossil use-package swoop sly rg projectile pod-mode perl6-mode paredit nord-theme markdown-mode+ magit fzf f elpher dumb-jump deadgrep csv-mode counsel company command-log-mode clojure-mode-extra-font-locking cider ace-jump-mode))
+ '(safe-local-variable-values
+   '((Base . 10)
+     (Package . FIVEAM)
+     (Syntax . Ansi-Common-Lisp))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#3F3F3F" :foreground "#DCDCCC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 170 :width normal :foundry "nil" :family "Iosevka"))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#2E3440" :foreground "#D8DEE9" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 140 :width normal :foundry "ADBO" :family "Iosevka"))))
  '(cider-repl-stderr-face ((t (:inherit font-lock-warning-face :weight normal))))
  '(cider-test-error-face ((t (:background "#d08770"))))
  '(cider-test-failure-face ((t (:background "#BF616A"))))
  '(cider-test-success-face ((t (:background "#A3BE8C" :foreground "black"))))
- '(font-lock-comment-face ((t (:foreground "#A3BE8C"))))
- '(font-lock-doc-face ((t (:foreground "#A3BE8C"))))
  '(markdown-code-face ((t (:inherit fixed-pitch :family "Iosevka"))))
  '(markdown-pre-face ((t (:inherit ##)))))
 
